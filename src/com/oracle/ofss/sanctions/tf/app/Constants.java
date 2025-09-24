1a4c398769d95197102fcc356bf0fb146c786bef
bmc_operator_access/suryas <suryansh.s@oracle.com>
2025-09-24 15:02:00 +0530

    utility jar support change

--- src/com/oracle/ofss/sanctions/tf/app/Constants.java
+++ src/com/oracle/ofss/sanctions/tf/app/Constants.java
@@ -1,8 +1,14 @@
 package com.oracle.ofss.sanctions.tf.app;
 
+import java.io.File;
+
 public class Constants {
 
-    public static final String CONFIG_FILE_PATH = "src/config.properties";
+    public static String CONFIG_FILE_NAME = "config";
+    public static String BIN_FOLDER_NAME = "bin";
+    public static String CURRENT_DIRECTORY = System.getProperty("user.dir");
+    public static File PARENT_DIRECTORY = new File(CURRENT_DIRECTORY).getParentFile();
+    public static String CONFIG_FILE_PATH = PARENT_DIRECTORY+File.separator+BIN_FOLDER_NAME+File.separator+CONFIG_FILE_NAME+".properties";
     public static final String FAIL_STATUS = "FAIL";
     public static final String SHEET_OS = "Open Search Issues";
     public static final String SHEET_OT = "Oracle Text Issues";
@@ -15,7 +21,6 @@ public class Constants {
     public static final String PROP_OT_COL = "TestStatusOT.column";
     public static final String PROP_OS_COLS = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,16";
     public static final String PROP_OT_COLS = "1,2,3,4,5,6,7,8,17,18,19,20,21,22,24";
-    public static final String PROP_RULE_NAME_COL = "ruleNameColumn";
     public static final String[] HEADERS = {
         "Rule Name", "Raw Message", "Tag", "Source Input", "Target Input",
         "Target Column", "Watchlist", "N_UID", "Transaction Token",
